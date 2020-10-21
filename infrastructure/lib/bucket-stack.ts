8f91ea6e3e9edd30f7a167db3a009a43f0be62ca
noce2 <20339399+noce2@users.noreply.github.com>
2020-10-21 22:04:49 +0100

    Add web ui bucket stack

---
+++ infrastructure/lib/bucket-stack.ts
@@ -0,0 +1,14 @@
+import * as core from '@aws-cdk/core';
+import * as s3 from '@aws-cdk/aws-s3';
+
+export class BucketCdkStack extends core.Stack {
+    constructor(scope: core.App, id: string, props?: core.StackProps) {
+        super(scope, id, props);
+
+        const bucket = new s3.Bucket(this, 'WebUIBucket', {
+            versioned: true,
+            websiteIndexDocument: 'index.html',
+            websiteErrorDocument: 'index.html'
+        })
+    }
+}

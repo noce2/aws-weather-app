8628d9640dfb5babf542dc4fbbdce04eb98e16f9
noce2 <20339399+noce2@users.noreply.github.com>
2020-10-21 22:05:06 +0100

    Set infrastructure stack to euw2

--- infrastructure/bin/infrastructure.ts
+++ infrastructure/bin/infrastructure.ts
@@ -1,6 +1,17 @@
 #!/usr/bin/env node
 import * as cdk from '@aws-cdk/core';
+import { Aws } from '@aws-cdk/core';
+import { BucketCdkStack } from '../lib/bucket-stack';
 import { InfrastructureStack } from '../lib/infrastructure-stack';
 
 const app = new cdk.App();
-new InfrastructureStack(app, 'InfrastructureStack');
+new InfrastructureStack(app, 'InfrastructureStack', {
+    env: {
+        region: 'eu-west-2'
+    }
+});
+new BucketCdkStack(app, 'WebUIStack', {
+    env: {
+        region: 'eu-west-2'
+    }
+});

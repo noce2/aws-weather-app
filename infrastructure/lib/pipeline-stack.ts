1adeb6dede0335abcf9802543687f187f1894023
noce2 <20339399+noce2@users.noreply.github.com>
2020-10-22 22:18:52 +0100

    Add pipeline stack

---
+++ infrastructure/lib/pipeline-stack.ts
@@ -0,0 +1,130 @@
+import * as codebuild from '@aws-cdk/aws-codebuild';
+import * as codecommit from '@aws-cdk/aws-codecommit';
+import * as codepipeline from '@aws-cdk/aws-codepipeline';
+import * as codepipeline_actions from '@aws-cdk/aws-codepipeline-actions';
+import * as lambda from '@aws-cdk/aws-lambda';
+import * as s3 from '@aws-cdk/aws-s3';
+import { App, Stack, StackProps, SecretValue } from '@aws-cdk/core';
+
+export interface PipelineStackProps extends StackProps {
+  readonly lambdaCode: lambda.CfnParametersCode;
+  readonly repoName: string
+}
+
+export class PipelineStack extends Stack {
+  constructor(app: App, id: string, props: PipelineStackProps) {
+    super(app, id, props);
+
+    const cdkBuild = new codebuild.PipelineProject(this, 'CdkBuild', {
+      buildSpec: codebuild.BuildSpec.fromObject({
+        version: '0.2',
+        phases: {
+          install: {
+            commands: [
+                'cd infrastructure',
+                'npm install'
+            ],
+          },
+          build: {
+            commands: [
+              'npm run build',
+              'npm run cdk synth -- -o dist'
+            ],
+          },
+        },
+        artifacts: {
+          'base-directory': 'infrastructure/dist',
+          files: [
+            'LambdaStack.template.json',
+          ],
+        },
+      }),
+      environment: {
+        buildImage: codebuild.LinuxBuildImage.STANDARD_2_0,
+      },
+    });
+    const lambdaBuild = new codebuild.PipelineProject(this, 'LambdaBuild', {
+      buildSpec: codebuild.BuildSpec.fromObject({
+        version: '0.2',
+        phases: {
+          install: {
+            commands: [
+              'cd backend',
+              'npm install',
+            ],
+          },
+          build: {
+            commands: 'npm run build',
+          },
+        },
+        artifacts: {
+          'base-directory': 'backend',
+          files: [
+            'index.js',
+            'node_modules/**/*',
+          ],
+        },
+      }),
+      environment: {
+        buildImage: codebuild.LinuxBuildImage.STANDARD_2_0,
+      },
+    });
+
+    const sourceOutput = new codepipeline.Artifact();
+    const cdkBuildOutput = new codepipeline.Artifact('CdkBuildOutput');
+    const lambdaBuildOutput = new codepipeline.Artifact('LambdaBuildOutput');
+    new codepipeline.Pipeline(this, 'Pipeline', {
+      stages: [
+        {
+          stageName: 'Source',
+          actions: [
+            new codepipeline_actions.GitHubSourceAction({
+                branch: 'main',
+                owner: 'noce2',
+                repo: 'aws-weather-app',
+                oauthToken: SecretValue.secretsManager(
+                    'arn:aws:secretsmanager:eu-west-2:892799438830:secret:prod/aws-weather-app/github-nYjmSc',
+                    {jsonField: 'github-access-token'}
+                    ),
+                trigger: codepipeline_actions.GitHubTrigger.WEBHOOK,
+                output: sourceOutput,
+                actionName: 'Clone aws-weather-app repo'
+              }),
+          ],
+        },
+        {
+          stageName: 'Build',
+          actions: [
+            new codepipeline_actions.CodeBuildAction({
+              actionName: 'Lambda_Build',
+              project: lambdaBuild,
+              input: sourceOutput,
+              outputs: [lambdaBuildOutput],
+            }),
+            new codepipeline_actions.CodeBuildAction({
+              actionName: 'CDK_Build',
+              project: cdkBuild,
+              input: sourceOutput,
+              outputs: [cdkBuildOutput],
+            }),
+          ],
+        },
+        {
+          stageName: 'Deploy',
+          actions: [
+            new codepipeline_actions.CloudFormationCreateUpdateStackAction({
+              actionName: 'Lambda_CFN_Deploy',
+              templatePath: cdkBuildOutput.atPath('LambdaStack.template.json'),
+              stackName: 'LambdaDeploymentStack',
+              adminPermissions: true,
+              parameterOverrides: {
+                ...props.lambdaCode.assign(lambdaBuildOutput.s3Location),
+              },
+              extraInputs: [lambdaBuildOutput],
+            }),
+          ],
+        },
+      ],
+    });
+  }
+}

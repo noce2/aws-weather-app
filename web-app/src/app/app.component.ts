aeef7642dbe3f651bdd3ae8380933a877633713d
noce2 <20339399+noce2@users.noreply.github.com>
2020-10-24 04:41:55 +0100

    Add http client to root web app component

--- web-app/src/app/app.component.ts
+++ web-app/src/app/app.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { BehaviorSubject, throwError } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'web-app';
+  temperatureSubject = new BehaviorSubject('');
+  dataReturned = this.temperatureSubject.value;
+  constructor(private http: HttpClient) {
+    http.get<string>(environment.apiUrl).subscribe({
+      next: (value) => this.temperatureSubject.next((value)),
+      error: this.handleError
+    });
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error.message);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong.
+      console.error(
+        `Backend returned code ${error.status}, ` +
+        `body was: ${JSON.stringify(error.error)}`);
+    }
+    // Return an observable with a user-facing error message.
+    return throwError(
+      'Something bad happened; please try again later.');
+  }
 }

d09be8430c1c756a4f8920ffb68e1e03618a11d2
noce2 <20339399+noce2@users.noreply.github.com>
2020-10-26 19:26:53 +0000

    Update function with handler

--- custom-resource-provider-lambda/index.py
+++ custom-resource-provider-lambda/index.py
@@ -1,3 +1,19 @@
+from crhelper import CfnResource
+import logging
+
+logger = logging.getLogger(__name__)
+# Initialise the helper, all inputs are optional, this example shows the defaults
+helper = CfnResource(json_logging=False, log_level='DEBUG', boto_level='CRITICAL', sleep_on_delete=120)
+
+@helper.create
+@helper.update
+def create(event, context):
+    logger.info("Got create handler!")
+    logger.info(f'Request id is: {event["RequestId"]}')
+    if (event["ResourceProperties"] == True):
+        return 'somestuff'
+
+
 def main(event, context):
     return {
         'statusCode': 200,

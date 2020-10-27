0704994a749d06862255c4e9fd9f518e75c37f87
noce2 <20339399+noce2@users.noreply.github.com>
2020-10-27 11:16:54 +0000

    Add policies to allow custom resource to deploy pipeline

--- infrastructure/lib/weather-app-custom-resource-provider-stack.ts
+++ infrastructure/lib/weather-app-custom-resource-provider-stack.ts
@@ -1,4 +1,5 @@
 import * as codedeploy from '@aws-cdk/aws-codedeploy';
+import { Effect, PolicyStatement } from '@aws-cdk/aws-iam';
 import * as lambda from '@aws-cdk/aws-lambda';
 import { App, CfnOutput, Duration, Stack, StackProps } from '@aws-cdk/core';
       
@@ -34,5 +35,64 @@ export class WeatherAppCustomResourceProviderStack extends Stack {
       alias,
       deploymentConfig: codedeploy.LambdaDeploymentConfig.ALL_AT_ONCE,
     });
+
+    const policyForCftManagement= new PolicyStatement({
+      sid: 'allowCustomResourceProviderAccessToCloudFormation',
+      effect: Effect.ALLOW,
+      resources: [
+        "*"
+      ],
+      actions: [
+        "cloudformation:*"
+      ]
+    });
+
+    const policyForKmsOperation= new PolicyStatement({
+      sid: 'allowCustomResourceProviderAccessToKms',
+      effect: Effect.ALLOW,
+      resources: [
+        "*"
+      ],
+      actions: [
+        "kms:CreateAlias",
+        "kms:CreateKey",
+        "kms:DeleteAlias",
+        "kms:Describe*",
+        "kms:GenerateRandom",
+        "kms:Get*",
+        "kms:List*",
+        "kms:TagResource",
+        "kms:UntagResource",
+        "iam:ListGroups",
+        "iam:ListRoles",
+        "iam:ListUsers"
+      ]
+    });
+
+    const policyForS3Operation= new PolicyStatement({
+      sid: 'allowCustomResourceProviderAccessToS3',
+      effect: Effect.ALLOW,
+      resources: [
+        "*"
+      ],
+      actions: [
+        "s3:*"
+      ]
+    });
+
+    const policyForIamRoleCreation= new PolicyStatement({
+      sid: 'allowCustomResourceProviderAccessToCreatePipelineIamRoles',
+      effect: Effect.ALLOW,
+      resources: [
+        "*"
+      ],
+      actions: [
+        "iam:*"
+      ]
+    });
+
+    func.addToRolePolicy(policyForCftManagement);
+    func.addToRolePolicy(policyForKmsOperation);
+    func.addToRolePolicy(policyForS3Operation);
   }
 }

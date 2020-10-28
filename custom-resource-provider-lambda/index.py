431226d88bcb937eaf804a935534141a274a225d
noce2 <20339399+noce2@users.noreply.github.com>
2020-10-28 08:07:51 +0000

    Add update handler for provider

--- custom-resource-provider-lambda/index.py
+++ custom-resource-provider-lambda/index.py
@@ -44,7 +44,36 @@ def create(event, context):
 
 
 @helper.update
-
+def update(event, context):
+    logger.info("Got update handler!")
+    logger.info(f'Request id is: {event["RequestId"]}')
+    try:
+        with open('PipelineDeployingLambdaStack.template.json') as pipelineTemplate:
+            if (("ResourceProperties" in event) and CUSTOM_RESOURCE_PROP_TO_CHECK in event['ResourceProperties']):
+                logger.info(f'branch to track will be: {event["ResourceProperties"][CUSTOM_RESOURCE_PROP_TO_CHECK]}')
+                response = cfClient.update_stack(
+                    StackName=event["PhysicalResourceId"],
+                    TemplateBody=json.dumps(json.load(pipelineTemplate)),
+                    Parameters=[
+                        {
+                            'ParameterKey': 'CdkAndAppSourceBranchToTrack',
+                            'ParameterValue': event["ResourceProperties"][CUSTOM_RESOURCE_PROP_TO_CHECK]
+                        },
+                        {
+                            'ParameterKey': 'IamUserToAllowKeyMolicyManagement',
+                            'ParameterValue': 'arn:aws:iam::892799438830:role/WeatherAppCustomResourceP-WeatherAppCustomResource-1OPPB682I99H1'
+                        },
+                    ],
+                    TimeoutInMinutes=13,
+                    Capabilities=[
+                        'CAPABILITY_NAMED_IAM',
+                    ]
+                )
+                return response['StackId']
+            else:
+                raise RuntimeError(f'{CUSTOM_RESOURCE_PROP_TO_CHECK} property not present. Instead found: {event["ResourceProperties"]}')
+    except OSError as identifier:
+        raise RuntimeError(f"could not read file because: {identifier.strerror}")
 
 @helper.delete
 def delete(event, context):

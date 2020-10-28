93bd142bb11fd211f9887a11f21b80512a9da51d
noce2 <20339399+noce2@users.noreply.github.com>
2020-10-28 09:41:26 +0000

    add axios call to open weather

--- backend/index.ts
+++ backend/index.ts
@@ -1,10 +1,24 @@
 import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult, Callback, Context } from "aws-lambda";
+import * as axios from 'axios';
 
 // index.ts
 const GREETING = "Hello, AWS!";
 
 export const main: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent, context: Context) => {
   console.log(GREETING);
+  const response = await axios.default.get('/data/2.5/weather?q=Nottingham&appid=f8f55f720d21aa96b080dd969e2d696e',
+  {
+    baseURL: 'api.openweathermap.org'
+  });
+  if(response && response.status === 200) {
+    return {
+      statusCode: 200,
+      body: JSON.stringify(response.data),
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      }
+    };
+  } 
   return {
     statusCode: 200,
     body: JSON.stringify({
